2fe190e2f3e3d3d8da6bf230e9322fa7c200ef98
akshay-ken <akshaybest3120@gmail.com>
2025-08-02 19:21:00 +0530

    style: created third item in design and added styles to it for mobile view

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,13 @@
 import { CardOne } from "./CardOne";
+import { CardThree } from "./CardThree";
 import { CardTwo } from "./CardTwo";
 
 export function MainPage() {
   return (
-    <main className="w-full h-screen p-4 gap-8 flex flex-col">
+    <main className="w-full h-screen p-4 my-12 gap-8 flex flex-col">
       <CardOne />
       <CardTwo />
+      <CardThree />
     </main>
   );
 }
